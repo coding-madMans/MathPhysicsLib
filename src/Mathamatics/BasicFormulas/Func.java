5fc0b9fbd27063d21cd0fb2bb5b4d4c0f8d35f76
vishalshetty104 <vishalshetty104@gmail.com>
2021-06-23 20:35:39 +0530

    added Merge Sort function

--- src/Mathamatics/BasicFormulas/Func.java
+++ src/Mathamatics/BasicFormulas/Func.java
@@ -5,52 +5,62 @@
 
 package Mathamatics.BasicFormulas;
 import Mathamatics.Numbers.Double;
-import java.math.*;
+
 import Mathamatics.MathematicalConstants;
+import Mathamatics.Numbers.Integer;
 import Mathamatics.Numbers.RealNumbers;
 import utility.MathError;
 
 public class Func {
 
     //BY VISHAL
-    public static Integer factorial(Integer num){
-        if(num==1 || num==0){
-            return 1;
+    public static Integer factorial(Integer num) {
+        if (num.getAsInt() == 1 || num.getAsInt() == 0) {
+            return new Integer(1);
         }
-        return (num*factorial(num-1));
+        Integer temp = new Integer(num.getAsInt() - 1);
+        try {
+            return new Integer(num.mul(factorial(temp)));
+        } catch (MathError mathError) {
+            mathError.printStackTrace();
+        }
     }
 
-    public static double Abs(double num)
-    {
-        if(num >= 0){
+    public static double Abs(double num) {
+        if (num >= 0) {
             return num;
         }
         return -num;
     }
 
-    public static RealNumbers Pow(RealNumbers base,RealNumbers power){
-        double ans=1.0;
+    public static RealNumbers Pow(RealNumbers base, RealNumbers power) {
+        double ans = 1.0;
         for (int i = 0; i < Abs(power.getAsInt()); i++) {
-                ans = ans * base.getAsDouble();
+            ans = ans * base.getAsDouble();
         }
 
-        if( power.getAsInt() < 0)
-        {
-            return new Double(1/ans);
+        if (power.getAsInt() < 0) {
+            return new Double(1 / ans);
         }
         return new Double(ans);
     }
 
-    public static double Sqrt(double number){
+    public static RealNumbers Sqrt(RealNumbers number) {
         double root = 1;
-        int i = 0;
-        while (true)
-        {
+        double i = 0;
+        while (true) {
             i = i + 1;
-            root = (number / root + root) / 2;
-            if (i == number + 1) { break; }
+            root = (number.getAsDouble() / root + root) / 2;
+            if (i == number.getAsDouble() + 1) {
+                break;
+            }
         }
-        return root;
+        return new Double(root);
+    }
+
+    public static RealNumbers Cbrt(RealNumbers number) {
+        Double power = new Double(1 / 3);
+        return Pow(number, power);
     }
 
     public static RealNumbers Sin(RealNumbers a) {
@@ -87,23 +97,26 @@ public class Func {
         // factorial() can handle it
         final int PRECISION = 100;
         double temp = 0, temp2 = temp;
-        int i = 0;
-        while((temp != MathematicalConstants.Negative_Infinity) && (i < PRECISION)){
+        //   int i = 0;
+        Integer i = new Integer(0);
+        while ((temp != MathematicalConstants.Negative_Infinity) && (i < PRECISION)) {
             temp2 = temp;
-            temp += Math.pow(-1, i) * (Math.pow(num, 2 * i + 1) / factorial(2 * i + 1));
+            temp += Pow(-1, i) * (Math.pow(num, 2 * i + 1) / factorial(2 * i + 1));
             i += 1;
         }
 
         return new Double(sign * temp2);
 
     }
-    public static RealNumbers Cos(RealNumbers deg){
-        return Sin(new Double(1.5708-deg.getAsDouble())); //1.5708 is 90 degrees in radians
+
+    public static RealNumbers Cos(RealNumbers deg) {
+        return Sin(new Double(1.5708 - deg.getAsDouble())); //1.5708 is 90 degrees in radians
     }
+
     public static RealNumbers Tan(RealNumbers deg) {
         Double sinValue = new Double(Sin(deg).getAsDouble());
         Double cosValue = new Double(Cos(deg).getAsDouble());
-        if(cosValue.getAsDouble()==0)
+        if (cosValue.getAsDouble() == 0)
             return new Double(MathematicalConstants.Positive_Infinity);
 
         try {
@@ -113,23 +126,26 @@ public class Func {
             return new Double(MathematicalConstants.NaN);
         }
     }
-    public static RealNumbers Cosec(RealNumbers deg){
+
+    public static RealNumbers Cosec(RealNumbers deg) {
         Double sinValue = new Double(Sin(deg).getAsDouble());
         if (sinValue.getAsDouble() == 0)
             return new Double(MathematicalConstants.Positive_Infinity);
 
-        return new Double(1/sinValue.getAsDouble());
+        return new Double(1 / sinValue.getAsDouble());
     }
-    public static RealNumbers Sec(RealNumbers deg){
+
+    public static RealNumbers Sec(RealNumbers deg) {
         Double cosValue = new Double(Cos(deg).getAsDouble());
         if (cosValue.getAsDouble() == 0)
             return new Double(MathematicalConstants.Positive_Infinity);
-        return new Double(1/cosValue.getAsDouble());
+        return new Double(1 / cosValue.getAsDouble());
     }
-    public static RealNumbers Cot(RealNumbers deg){
+
+    public static RealNumbers Cot(RealNumbers deg) {
         Double sinValue = new Double(Sin(deg).getAsDouble());
         Double cosValue = new Double(Cos(deg).getAsDouble());
-        if(sinValue.getAsDouble()==0)
+        if (sinValue.getAsDouble() == 0)
             return new Double(MathematicalConstants.Positive_Infinity);
 
         try {
@@ -143,74 +159,129 @@ public class Func {
 
     // BY SUPRITH
 
-    public static RealNumbers Exp(RealNumbers num)
-    {
-        return Pow(new Double(MathematicalConstants.e),num);
+    public static RealNumbers Exp(RealNumbers num) {
+        return Pow(new Double(MathematicalConstants.e), num);
     }
 
-    public static RealNumbers Sinh(RealNumbers deg)
-    {
+    public static RealNumbers Sinh(RealNumbers deg) {
         //Double Value1 = new Double(Exp(deg).getAsDouble());
         //Double Value2 = new Double(Exp(new Double(deg.inv())).getAsDouble());
-        return new Double((Exp(deg).getAsDouble()-(Exp(new Double(-deg.getAsDouble()))).getAsDouble())/2);
+        return new Double((Exp(deg).getAsDouble() - (Exp(new Double(-deg.getAsDouble()))).getAsDouble()) / 2);
     }
 
-    public static RealNumbers Cosh(RealNumbers deg)
-    {
-        return new Double((Exp(deg).getAsDouble()+(Exp(new Double(-deg.getAsDouble()))).getAsDouble())/2);
+    public static RealNumbers Cosh(RealNumbers deg) {
+        return new Double((Exp(deg).getAsDouble() + (Exp(new Double(-deg.getAsDouble()))).getAsDouble()) / 2);
     }
 
-    public static RealNumbers Tanh(RealNumbers deg)
-    {
+    public static RealNumbers Tanh(RealNumbers deg) {
         Double sinValue = new Double(Sinh(deg).getAsDouble());
         Double cosValue = new Double(Cosh(deg).getAsDouble());
-        if(Cosh(deg).getAsDouble() == 0)
-        {
+        if (Cosh(deg).getAsDouble() == 0) {
             return new Double(MathematicalConstants.Positive_Infinity);
         }
         try {
             return (RealNumbers) sinValue.div(cosValue);
-        }
-        catch (MathError mathError)
-        {
+        } catch (MathError mathError) {
             mathError.printStackTrace();
             return new Double(MathematicalConstants.NaN);
         }
     }
 
-    public static RealNumbers Cosech(RealNumbers deg)
-    {
+    public static RealNumbers Cosech(RealNumbers deg) {
         Double sinhValue = new Double(Sinh(deg).getAsDouble());
-        if (sinhValue.getAsDouble() == 0){
+        if (sinhValue.getAsDouble() == 0) {
             return new Double(MathematicalConstants.Positive_Infinity);
         }
-        return new Double(1/sinhValue.getAsDouble());
+        return new Double(1 / sinhValue.getAsDouble());
     }
 
-    public static RealNumbers Sech(RealNumbers deg)
-    {
+    public static RealNumbers Sech(RealNumbers deg) {
         Double coshValue = new Double(Cosh(deg).getAsDouble());
-        if (coshValue.getAsDouble() == 0){
+        if (coshValue.getAsDouble() == 0) {
             return new Double(MathematicalConstants.Positive_Infinity);
         }
-        return new Double(1/coshValue.getAsDouble());
+        return new Double(1 / coshValue.getAsDouble());
     }
 
-    public static RealNumbers Coth(RealNumbers deg)
-    {
+    public static RealNumbers Coth(RealNumbers deg) {
         Double sinValue = new Double(Sinh(deg).getAsDouble());
         Double cosValue = new Double(Cosh(deg).getAsDouble());
-        if(Sinh(deg).getAsDouble() == 0)
-        {
+        if (Sinh(deg).getAsDouble() == 0) {
             return new Double(MathematicalConstants.Positive_Infinity);
         }
         try {
             return (RealNumbers) cosValue.div(sinValue);
-        }
-        catch (MathError mathError)
-        {
+        } catch (MathError mathError) {
             mathError.printStackTrace();
             return new Double(MathematicalConstants.NaN);
         }
     }
+
+    //MERGE SORT BEGINS
+    public static void merge(RealNumbers arr[], int l, int m, int r) {
+        // Find sizes of two sub-arrays to be merged
+        int n1 = m - l + 1;
+        int n2 = r - m;
+
+        /* Create temp arrays */
+        RealNumbers L[] = new RealNumbers[n1];
+        RealNumbers R[] = new RealNumbers[n2];
+
+        /*Copy data to temp arrays*/
+        for (int i = 0; i < n1; ++i)
+            L[i] = arr[l + i];
+        for (int j = 0; j < n2; ++j)
+            R[j] = arr[m + 1 + j];
+
+        /* Merge the temp arrays */
+
+        // Initial indexes of first and second subarrays
+        int i = 0, j = 0;
+
+        // Initial index of merged subarry array
+        int k = l;
+        while (i < n1 && j < n2) {
+            if (L[i].getAsDouble() <= R[j].getAsDouble()) {
+                arr[k] = L[i];
+                i++;
+            } else {
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+
+        /* Copy remaining elements of L[] if any */
+        while (i < n1) {
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
+
+        /* Copy remaining elements of R[] if any */
+        while (j < n2) {
+            arr[k] = R[j];
+            j++;
+            k++;
+        }
+    }
+
+    // Main function that sorts arr[l..r] using
+    // merge()
+    public static RealNumbers[] Sort(RealNumbers[] arr, int l, int r) {
+        if (l < r) {
+            // Find the middle point
+            int m = l + (r - l) / 2;
+
+            // Sort first and second halves
+            Sort(arr, l, m);
+            Sort(arr, m + 1, r);
+
+            // Merge the sorted halves
+            merge(arr, l, m, r);
+        }
+        return arr;
+
+    }
+    //MERGE SORT END
 }
